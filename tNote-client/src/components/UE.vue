c0636bab2589c3ae621d8fd41b4e0d7b5a361e16
tz <3128379695@qq.com>
2019-06-20 18:28:54 +0800

    代码规范修改

--- tNote-client/src/components/UE.vue
+++ tNote-client/src/components/UE.vue
@@ -4,31 +4,31 @@
     </div>
 </template>
 <script>
-    export default {
-        name: 'ue',
-        data () {
-            return {
-                editor: null
-            }
-        },
-        props: {
-            value: '',
-            config: {}
-        },
-        mounted () {
-            this.editor = window.UE.getEditor('editor', this.config);
-            this.editor.addListener('ready',  () => {
-                this.editor.setContent(this.value)
-            })
-        },
-        methods: {
-            getUEContent () {
-                //console.log(this.editor,getContent())
-                return this.editor.getContent()
-            }
-        },
-        destroyed () {
-            this.editor.destroy()
-        }
+export default {
+  name: 'ue',
+  data () {
+    return {
+      editor: null
     }
+  },
+  props: {
+    value: '',
+    config: {}
+  },
+  mounted () {
+    this.editor = window.UE.getEditor('editor', this.config);
+    this.editor.addListener('ready', () => {
+      this.editor.setContent(this.value)
+    })
+  },
+  methods: {
+    getUEContent () {
+      // console.log(this.editor,getContent())
+      return this.editor.getContent()
+    }
+  },
+  destroyed () {
+    this.editor.destroy()
+  }
+}
 </script>
