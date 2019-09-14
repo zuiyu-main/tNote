3286fbe0d3a44e42688ff438c66377b021dd7e55
tz <3128379695@qq.com>
2019-09-14 10:42:16 +0800

    登录页面添加，动态视频，

---
+++ tNote-client/src/pages/Login.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="homepage-hero-module">
+    <div class="video-container">
+      <div :style="fixStyle" class="filter"></div>
+      <video :style="fixStyle" autoplay loop class="fillWidth" v-on:canplay="canplay">
+        <source src="../video/MP4/Sunset-Siesta.mp4" type="video/mp4" />浏览器不支持 video 标签，建议升级浏览器。
+        <source src="../video/WEBM/Sunset-Siesta.webm" type="video/webm" />浏览器不支持 video 标签，建议升级浏览器。
+      </video>
+      <div class="poster hidden" v-if="!vedioCanPlay">
+        <img :style="fixStyle" src="../video/Snapshots/Sunset-Siesta.jpg" alt />
+      </div>
+      <div class="login-form">
+        <el-form ref="form" :model="form" label-width="80px">
+          <el-form-item label="用户名">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="密码">
+            <el-input v-model="form.password" type="password"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-radio-group v-model="form.resource">
+              <el-radio label="自动登录"></el-radio>
+              <el-radio label="记住密码"></el-radio>
+            </el-radio-group>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="onSubmit">立即创建</el-button>
+            <el-button>取消</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  components: {},
+  data() {
+    return {
+      // 控制播放视频
+      vedioCanPlay: false,
+      fixStyle: '',
+      form: {
+        name: '',
+        password: '',
+        resource: ''
+      }
+    }
+  },
+  methods: {
+    canplay() {
+      this.vedioCanPlay = true
+    },
+    onSubmit() {
+      console.log('submit!', this.form)
+    }
+  },
+  mounted: function() {
+    window.onresize = () => {
+      const windowWidth = document.body.clientWidth
+      const windowHeight = document.body.clientHeight
+      const windowAspectRatio = windowHeight / windowWidth
+      let videoWidth
+      let videoHeight
+      if (windowAspectRatio < 0.5625) {
+        videoWidth = windowWidth
+        videoHeight = videoWidth * 0.5625
+        this.fixStyle = {
+          height: windowWidth * 0.5625 + 'px',
+          width: windowWidth + 'px',
+          'margin-bottom': (windowHeight - videoHeight) / 2 + 'px',
+          'margin-left': 'initial'
+        }
+      } else {
+        videoHeight = windowHeight
+        videoWidth = videoHeight / 0.5625
+        this.fixStyle = {
+          height: windowHeight + 'px',
+          width: windowHeight / 0.5625 + 'px',
+          'margin-left': (windowWidth - videoWidth) / 2 + 'px',
+          'margin-bottom': 'initial'
+        }
+      }
+    }
+    window.onresize()
+  }
+}
+</script>
+<style scoped>
+.homepage-hero-module,
+.video-container {
+  position: relative;
+  height: 100vh;
+  overflow: hidden;
+}
+
+.video-container .poster img,
+.video-container video {
+  z-index: 0;
+  position: absolute;
+}
+
+.video-container .filter {
+  z-index: 1;
+  position: absolute;
+  background: rgba(0, 0, 0, 0.4);
+}
+.login-form {
+  position: absolute;
+  top: 33%;
+  left: 37%;
+  z-index: 1;
+}
+</style>
--- tNote-client/src/router/index.js
+++ tNote-client/src/router/index.js
@@ -3,12 +3,18 @@ import Router from 'vue-router'
 import markdown from '@/components/markdown'
 import UEdit from '@/components/UEdit'
 import Index from '@/pages/MyIndex'
+import Login from '@/pages/Login'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/edit',
       name: 'myIndex',
       component: Index,
       redirect: '/markdown',
